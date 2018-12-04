5218f6d7467b8c5088fc68c36bc21ddc41554fed
Lajus <lajusti@gmail.com>
2018-12-04 21:22:07 +0100

    first commit

---
+++ src/main/java/com/alejandro/lajusticia/codechallenge2018/CodeChallenge2018Application.java
@@ -0,0 +1,12 @@
+package com.alejandro.lajusticia.codechallenge2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallenge2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallenge2018Application.class, args);
+	}
+}
